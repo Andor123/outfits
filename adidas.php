671a2f218d6fdc8af286a6ebc3926b22b12a776a
Salamon Andor <salamon.andor@gmail.com>
2018-06-04 16:52:36 +0200

    Add files to new repository Signed-off-by: Salamon Andor <salamon.andor@gmail.com>

---
+++ adidas.php
@@ -0,0 +1,103 @@
+<?php
+include "config.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Adidas</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
+          integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4"
+          crossorigin="anonymous">
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+<header>
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light" style="background-color: lightblue;">
+        <a class="navbar-brand" href="index.php">Outfits</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                data-target="#navbarContent" aria-controls="navbarContent"
+                aria-expanded="false" aria-label="Toggle Navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="hummel.php">Hummel</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="macron.php">Macron</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="adidas.php">Adidas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="nike.php">Nike</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="puma.php">Puma</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="admiral.php">Admiral</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="errea.php">Errea</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="marathon.php">Marathon</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="kappa.php">Kappa</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="unknown.php">Unknown</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
+<section class="container-fluid">
+    <div class="container3">
+        <h1 class="h1">Countries with Adidas outfit</h1>
+        <table border="1" align="center">
+            <thead>
+            <tr>
+                <th>Country Name</th>
+                <th>Sport Name</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            $sql = "SELECT country_name, sport_name, brand_name FROM outfit_list WHERE brand_name = 'Adidas' ORDER BY country_name ASC";
+            $result = mysqli_query($connection, $sql);
+
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_array($result)) {
+                    ?>
+                    <tr>
+                        <td><?php echo $row["country_name"]; ?></td>
+                        <td><?php echo $row["sport_name"]; ?></td>
+                    </tr>
+                    <?php
+                }
+            }
+            ?>
+            </tbody>
+        </table>
+    </div>
+</section>
+<footer class="footer">
+    <span class="text-center">&copy;2018, Salamon Andor</span>
+</footer>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"
+        integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"
+        integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm"
+        crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Outfits</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
+          integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4"
+          crossorigin="anonymous">
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+<header>
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light" style="background-color: lightblue;">
+        <a class="navbar-brand" href="index.php">Outfits</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                data-target="#navbarContent" aria-controls="navbarContent"
+                aria-expanded="false" aria-label="Toggle Navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="hummel.php">Hummel</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="macron.php">Macron</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="adidas.php">Adidas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="nike.php">Nike</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="puma.php">Puma</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="admiral.php">Admiral</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="errea.php">Errea</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="marathon.php">Marathon</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="kappa.php">Kappa</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="unknown.php">Unknown</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
+<section class="container-fluid">
+    <div class="container1">
+        <h1 class="h1">Welcome to my Outfits list website!</h1>
+        <p class="lead">In this site I am going to list all the teams and nations by outfits.</p>
+    </div>
+</section>
+<section class="container-fluid">
+    <div class="container2">
+        <div class="row">
+            <div class="col-sm">
+                <img src="images/hummel.png" class="rounded" alt="hummel"><br>
+                <a class="btn btn-primary" href="hummel.php">Hummel</a>
+            </div>
+            <div class="col-sm">
+                <img src="images/macron.png" class="rounded" alt="macron"><br>
+                <a class="btn btn-primary" href="macron.php">Macron</a>
+            </div>
+            <div class="col-sm">
+                <img src="images/adidas.png" class="rounded" alt="adidas"><br>
+                <a class="btn btn-primary" href="adidas.php">Adidas</a>
+            </div>
+            <div class="col-sm">
+                <img src="images/nike.png" class="rounded" alt="nike"><br>
+                <a class="btn btn-primary" href="nike.php">Nike</a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm">
+                <img src="images/puma.png" class="rounded" alt="puma"><br>
+                <a class="btn btn-primary" href="puma.php">Puma</a>
+            </div>
+            <div class="col-sm">
+                <img src="images/admiral.png" class="rounded" alt="admiral"><br>
+                <a class="btn btn-primary" href="admiral.php">Admiral</a>
+            </div>
+            <div class="col-sm">
+                <img src="images/errea.png" class="rounded" alt="erreá"><br>
+                <a class="btn btn-primary" href="errea.php">Errea</a>
+            </div>
+            <div class="col-sm">
+                <img src="images/marathon.png" class="rounded" alt="marathon"><br>
+                <a class="btn btn-primary" href="marathon.php">Marathon</a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm">
+                <img src="images/kappa.png" class="rounded" alt="kappa"><br>
+                <a class="btn btn-primary" href="kappa.php">Kappa</a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm">
+                <img src="images/unknown.png" class="rounded" alt="unknown"><br>
+                <a class="btn btn-primary" href="unknown.php">Unknown</a>
+            </div>
+        </div>
+    </div>
+</section>
+<footer class="footer">
+    <span class="text-center">&copy;2018, Salamon Andor</span>
+</footer>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"
+        integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"
+        integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm"
+        crossorigin="anonymous"></script>
+</body>
+</html>
